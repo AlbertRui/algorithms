18f3318427191139a7dccbcac5ec73f1c0f55415
AlbertRui <ZR_sxlx@163.com>
2018-03-17 14:45:36 +0800

    add the selection sort

---
+++ java/Sorting-Basic/Selection-Sort/SelectionSort.java
@@ -0,0 +1,32 @@
+/**
+ * @author AlbertRui
+ * @date 2018-03-17 12:33
+ */
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i ++){
+            //寻找（i， n）区间里的最小值的索引
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minIndex] > arr[j]) {
+                    minIndex = j;
+                }
+            }
+            swap(arr, i, minIndex);
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        selectionSort(arr);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
