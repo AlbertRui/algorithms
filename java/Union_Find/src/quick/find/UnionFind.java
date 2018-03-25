f895b4a147d0467f2bdb2ce4498b9a81ee87877e
AlbertRui <isruizhang@gmail.com>
2018-03-25 18:22:32 +0800

    complete the base union find

--- java/Union_Find/src/quick/find/UnionFind.java
+++ java/Union_Find/src/quick/find/UnionFind.java
@@ -1,8 +1,67 @@
 package quick.find;
 
 /**
+ * 并查集
+ *
  * @author AlbertRui
  * @date 2018-03-25 17:51
  */
+@SuppressWarnings({"javadoc", "unused"})
 public class UnionFind {
+
+    //并查集数组
+    private int[] id;
+    //数组元素个数
+    private int count;
+
+    public UnionFind(int count) {
+        this.count = count;
+        this.id = new int[count];
+        //冰茶元素初始化
+        for (int i = 0; i < id.length; i++) {
+            id[i] = i;
+        }
+    }
+
+    /**
+     * 查找元素p所对应的集合编号
+     *
+     * @param p
+     * @return
+     */
+    public int find(int p) {
+        assert (p >= 0 && p < count);
+        return id[p];
+    }
+
+    /**
+     * 查看元素p和q是否所属一个集合
+     *
+     * @param p
+     * @param q
+     * @return
+     */
+    public boolean isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    /**
+     * 合并元素p和q所属集合
+     *
+     * @param p
+     * @param q
+     */
+    public void unionElements(int p, int q) {
+        int pId = find(p);
+        int qId = find(q);
+        if (pId == qId) {
+            return;
+        }
+        for (int i = 0; i < count; i++) {
+            if (id[i] == pId) {
+                id[i] = qId;
+            }
+        }
+    }
+
 }
