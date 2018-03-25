23dccf97fcc6b9cde5888a814b274ecdd1c82ff0
AlbertRui <isruizhang@gmail.com>
2018-03-25 20:36:10 +0800

    add optimization for the union find

--- java/Union_Find/src/quick/union/UnionFind.java
+++ java/Union_Find/src/quick/union/UnionFind.java
@@ -1,42 +1,44 @@
-package quick.find;
+package quick.union;
 
 /**
- * 并查集
+ * 利用树实现并查集
  *
  * @author AlbertRui
- * @date 2018-03-25 17:51
+ * @date 2018-03-25 19:22
  */
 @SuppressWarnings({"javadoc", "unused"})
 public class UnionFind {
 
-    //并查集数组
-    private int[] id;
-    //数组元素个数
+    //并查元素集
+    private int[] parent;
+    //并查元素个数
     private int count;
 
     public UnionFind(int count) {
         this.count = count;
-        this.id = new int[count];
-        //并查元素初始化
-        for (int i = 0; i < id.length; i++) {
-            id[i] = i;
+        this.parent = new int[count];
+        for (int i = 0; i < count; i++) {
+            parent[i] = i;
         }
     }
 
     /**
-     * 查找元素p所对应的集合编号
+     * 并查元素的查找
      *
      * @param p
      * @return
      */
-    @SuppressWarnings("ALL")
+    @SuppressWarnings(value = "ALL")
     public int find(int p) {
         assert (p >= 0 && p < count);
-        return id[p];
+        while (p != parent[p]) {
+            p = parent[p];
+        }
+        return p;
     }
 
     /**
-     * 查看元素p和q是否所属一个集合
+     * 检查两个并查元素是否连接在一起
      *
      * @param p
      * @param q
@@ -47,22 +49,18 @@ public class UnionFind {
     }
 
     /**
-     * 合并元素p和q所属集合
+     * 将p和q所对应的根连接在一起
      *
      * @param p
      * @param q
      */
     public void unionElements(int p, int q) {
-        int pId = find(p);
-        int qId = find(q);
-        if (pId == qId) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+        if (pRoot == qRoot) {
             return;
         }
-        for (int i = 0; i < count; i++) {
-            if (id[i] == pId) {
-                id[i] = qId;
-            }
-        }
+        parent[pRoot] = qRoot;
     }
 
 }
