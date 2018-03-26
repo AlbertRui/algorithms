36fca2f6cbc2ac3730ed02b9a9d6f1b8da5deec1
AlbertRui <isruizhang@gmail.com>
2018-03-26 20:21:35 +0800

    update read graph

--- java/Graph-Basic/src/graph/read/Main.java
+++ java/Graph-Basic/src/graph/read/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) {
 
         // 使用两种图的存储方式读取testG1.txt文件
-        String filename = "java/Graph-Basic/testG1.txt";
+        String filename = "java/Graph-Basic/resources/testG1.txt";
         SparseGraph g1 = new SparseGraph(13, false);
         ReadGraph readGraph1 = new ReadGraph(g1, filename);
         System.out.println("test G1 in Sparse Graph:");
@@ -28,7 +28,7 @@ public class Main {
         System.out.println();
 
         // 使用两种图的存储方式读取testG2.txt文件
-        filename = "java/Graph-Basic/testG2.txt";
+        filename = "java/Graph-Basic/resources/testG2.txt";
         SparseGraph g3 = new SparseGraph(6, false);
         ReadGraph readGraph3 = new ReadGraph(g3, filename);
         System.out.println("test G2 in Sparse Graph:");
